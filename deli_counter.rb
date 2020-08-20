11ae91b91b0b707d92670fba6c254980126c4e2e
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-08-20 13:25:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.empty?
-  puts "The line is currently empty."
-else
-end
+    puts "The line is currently empty?"
+  else
+  end
 end
 
 def take_a_number(array, string)
