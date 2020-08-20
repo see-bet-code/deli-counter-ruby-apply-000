b270b40b0a5482df01cb6a1ef61ef23426d031b0
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-08-20 13:22:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  puts "The line is currently empty."
+end
+
+def take_a_number(array, string)
+  line(array)
+end
+
+def now_serving
+  person = katz_deli.shift()
+  puts "Currently serving #{person}"
+end
