b53f7908a23323c7aebb8962bfb5d643eb2268a4
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-08-20 13:57:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,10 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     line = "The line is currently:"
-    #katz_deli.each do |name|
-    #  puts ""
+    katz_deli.each do |name|
+      line = line + "#{katz_deli.index(name) + 1}. #{name}"
+    end
+    puts line
   end
 end
 
